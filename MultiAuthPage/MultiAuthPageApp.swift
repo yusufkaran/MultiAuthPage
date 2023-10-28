62c778505344b6bdd2c7586d3e247f84af5c09d1
Yusuf Karan <karan.yusuf.20@gmail.com>
2023-10-28 17:38:13 +0300

    Initial Commit

---
+++ MultiAuthPage/MultiAuthPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiAuthPageApp.swift
+//  MultiAuthPage
+//
+//  Created by Yusuf Karan on 28.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MultiAuthPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
